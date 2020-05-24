98be1b10958e33a6a2a479c28e21711b956e7514
Jackson Torres <62389221+jacksontorreseconomista@users.noreply.github.com>
2020-05-24 17:04:30 -0300

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,32 @@
+#Reading Data 
+
+datafile <- '/Volumes/NOSKAJ/Dropbox/07 PESQUISAS/R/COURSES/COUSERA/Data Science Specialization/Exploratory Data Analysis/project 1/household_power_consumption.txt'
+powerdata <- read.table(datafile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+#subsetingdata
+powerdata_sb <- subset(powerdata, Date %in% c("1/2/2007","2/2/2007"))
+
+
+#manipulating data
+ 
+powerdata_sb <- 
+  transform(powerdata_sb, 
+            Date = as.Date(Date, format="%d/%m/%Y"),
+            Global_active_power = as.numeric(Global_active_power),
+            Global_reactive_power = as.numeric(Global_reactive_power),
+            Global_intensity = as.numeric(Global_intensity),
+            Voltage = as.numeric(Voltage),
+            Sub_metering_1 = as.numeric(Sub_metering_1),
+            Sub_metering_2 = as.numeric(Sub_metering_2),
+            Sub_metering_3 = as.numeric(Sub_metering_3))
+datetime <- paste(powerdata_sb$Date, powerdata_sb$Time)
+powerdata_sb$Datetime <- as.POSIXct(datetime)
+
+str(powerdata_sb)
+
+#Plot1
+
+hist(powerdata_sb$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
