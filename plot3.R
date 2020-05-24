52fb837a9584c328b84548ef0eb52b3b6844f7de
Jackson Torres <62389221+jacksontorreseconomista@users.noreply.github.com>
2020-05-24 17:06:39 -0300

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,39 @@
+#Reading Data 
+
+datafile <- '/Volumes/NOSKAJ/Dropbox/07 PESQUISAS/R/COURSES/COUSERA/Data Science Specialization/Exploratory Data Analysis/project 1/household_power_consumption.txt'
+powerdata <- read.table(datafile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+#subsetingdata
+powerdata_sb <- subset(powerdata, Date %in% c("1/2/2007","2/2/2007"))
+
+
+#manipulating data
+ 
+powerdata_sb <- 
+  transform(powerdata_sb, 
+            Date = as.Date(Date, format="%d/%m/%Y"),
+            Global_active_power = as.numeric(Global_active_power),
+            Global_reactive_power = as.numeric(Global_reactive_power),
+            Global_intensity = as.numeric(Global_intensity),
+            Voltage = as.numeric(Voltage),
+            Sub_metering_1 = as.numeric(Sub_metering_1),
+            Sub_metering_2 = as.numeric(Sub_metering_2),
+            Sub_metering_3 = as.numeric(Sub_metering_3))
+datetime <- paste(powerdata_sb$Date, powerdata_sb$Time)
+powerdata_sb$Datetime <- as.POSIXct(datetime)
+
+str(powerdata_sb)
+
+#Plot 3
+with(powerdata_sb, {
+  plot(Sub_metering_1 ~ Datetime, type="l",
+       ylab="Global Active Power (kilowatts)", xlab="")
+  lines(Sub_metering_2~Datetime,col='Red')
+  lines(Sub_metering_3~Datetime,col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
+
+
